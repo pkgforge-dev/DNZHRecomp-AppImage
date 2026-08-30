b107f2235f1fb474d78454e17081804788e2dff0
Link4Electronics <link4electronics@gmail.com>
2026-08-30 03:58:34 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,8 +10,6 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=https://gitlab.com/sonicdcer/DNZHRecomp/-/raw/main/icons/512.png?ref_type=heads
 export DESKTOP=https://gitlab.com/sonicdcer/DNZHRecomp/-/raw/main/.github/linux/DNZHRecompiled.desktop?ref_type=heads
 export STARTUPWMCLASS=DNZHRecompiled
-export DEPLOY_GTK=1
-export GTK_DIR=gtk-3.0
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
